054848d9dc4bd6efb58b13c43efa00e36ee8c402
Alik Dev <moisesalik01@gmail.com>
2023-04-03 10:19:37 -0500

    beta 0.2

--- src/app/modules/home/pages/about/about.component.ts
+++ src/app/modules/home/pages/about/about.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Blog } from 'src/app/interfaces/responses.interface';
+import { BlogService } from 'src/app/services/blog.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html'
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public blog!: Blog
+
+  public sub!: Subscription
+
+  constructor(
+    private blogService: BlogService
+  ) { }
 
   ngOnInit(): void {
+
+    this.sub = this.blogService.getBlogByID('642ad28cd903e01e992a2408').subscribe({
+      next: resp => {
+        this.blog = resp.blog;
+      }
+    })
+
+  }
+
+  ngOnDestroy(): void {
+    if(this.sub)
+      this.sub.unsubscribe();
   }
 
 }
